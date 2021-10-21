33d4ede1a05834b3faee63903272971db74999e6
Oscar <a127309512@gmail.com>
2021-10-21 23:36:36 +0800

    # UserDetails InMemory userDetails manager

--- src/main/java/com/example/SpringSecurityDemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/SpringSecurityDemo/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.SpringSecurityDemo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity // the place config everything has to do with security for application
@@ -18,5 +23,20 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    // how treating user from DataBase
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annSmithUser = User.builder()
+                .username("annasmith")
+                .password("password")
+                .roles("STUDENT") // ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annSmithUser
+        );
     }
 }

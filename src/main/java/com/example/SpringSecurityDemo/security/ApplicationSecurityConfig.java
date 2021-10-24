2ef0da4dee8612763a0c6ee3dda7ef5d2ff7eea8
Oscar <a127309512@gmail.com>
2021-10-24 17:25:05 +0800

    # hasAuthority 設定對於/management/api/** API進行permission設定 Create, Update, Delete : 對於有COURSE_WRITE權限的人可以執行 Read : ADMIN, ADMINTRAINEE角色的人可以執行

--- src/main/java/com/example/SpringSecurityDemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/SpringSecurityDemo/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.SpringSecurityDemo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.SpringSecurityDemo.security.ApplicationUserPermission.*;
 import static com.example.SpringSecurityDemo.security.ApplicationUserRole.*;
 
 @Configuration
@@ -30,6 +32,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/","index","/css/*","/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()

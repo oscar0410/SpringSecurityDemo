725bf59f28e58d5365c936909cac889e43464b47
Oscar <a127309512@gmail.com>
2021-10-24 20:02:55 +0800

    # 利用Annotation方式，取得antMatcher指定特定的API樣式以及可以存取的角色&權限 ApplicationSecurityConfig : @EnableGlobalMethodSecurity新增 StudentManagementController : 在每個方法上新增@PreAuthorize

--- src/main/java/com/example/SpringSecurityDemo/student/StudentManagementController.java
+++ src/main/java/com/example/SpringSecurityDemo/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.example.SpringSecurityDemo.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -14,25 +15,31 @@ public class StudentManagementController {
             new Student(2, "Maria Jones"),
             new Student(3, "Anna Smith")
     );
+//    With permisson, using authorisation on method level --> @PreAuthorize
+//    hasRole('ROLE_'), hasAnyRole('ROLE_'), hasAuthority('permission'), hasAnyAuthority('permission')
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println("Register New Student :");
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println("Delete student :");
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
         System.out.println("Update Student :");
         System.out.printf("%s %s%n", studentId, student);
